ff558eb49fa61cb953c99f0fee1e866f882e0ef5
enes-uzunoglu <developer.enesuzunoglu@gmail.com>
2025-04-26 18:12:54 +0300

    main branch arrangment

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,12 @@
 
 import React from "react";
-import TopProduct from "../components/TopProduct";
-import BestSellerProducts from "../components/BestSellerProducts";
-import Clients from "../components/Clients";
-import Slogan from "../components/Slogan";
-import FeaturedPosts from "../components/FeaturedPosts";
-import BestServices from "../components/BestServices";
-import Slider from "../components/Slider";
+import TopProduct from "@/components/TopProduct";
+import BestSellerProducts from "@/components/BestSellerProducts";
+import Clients from "@/components/Clients";
+import Slogan from "@/components/Slogan";
+import FeaturedPosts from "@/components/FeaturedPosts";
+import BestServices from "@/components/BestServices";
+import Slider from "@/components/Slider";
 
 const Homepage: React.FC = () => {
   return (
