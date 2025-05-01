37a0650461dec97e1f0c81dcf56197809d095586
enes-uzunoglu <developer.enesuzunoglu@gmail.com>
2025-05-01 17:35:35 +0300

    gereksiz use client kullanımı footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,7 @@
-'use client';
+
 import React from 'react';
 import { Facebook, Instagram, Twitter, Youtube } from 'lucide-react';
 import Link from 'next/link';
-import { useSelector, useDispatch } from 'react-redux';
 
 const Footer: React.FC = () => {
   return (
