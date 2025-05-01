39494f136b6c9113507fb3370e777d55864ed78a
enes-uzunoglu <developer.enesuzunoglu@gmail.com>
2025-05-01 16:43:51 +0300

    VERCEL HATALARI DÜZELTİLDİ.

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['picsum.photos','via.placeholder.com','workintech-fe-ecommerce.onrender.com','cdn.dsmcdn.com'], // Picsum photos domaini eklendi
+    domains: ['picsum.photos', 'via.placeholder.com', 'workintech-fe-ecommerce.onrender.com', 'cdn.dsmcdn.com'], // Picsum photos domaini eklendi
   },
+  // SSR sırasında Redux sorunlarını çözmek için:
+  output: "export", // veya bu satırı kaldırıp sadece mevcut ayarları kullanabilirsiniz
 };
 
 export default nextConfig;
